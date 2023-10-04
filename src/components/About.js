cfb4a26d36638d5bcde3738cf17c0176d950a223
Jnanesh <jnaneshps4@gmail.com>
2023-10-04 19:59:38 +0530

    change2

--- src/components/About.js
+++ src/components/About.js
@@ -37,7 +37,7 @@ export default function About(props) {
           >
             <div className="accordion-body" style={myStyle}>
               Textutils gives you a way to analyze your text quickly and
-              efficiently. Be it word count, character count
+              efficiently. Be it word count, character counts
             </div>
           </div>
         </div>
